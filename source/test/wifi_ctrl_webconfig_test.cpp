87c93ace2fd6cee1a4eafd10b69b08e1b58d99bc
Fritz Heckel <fritz@permanence.ai>
2025-09-03 02:53:59 +0000

    basic tests for webconfig_steering_config_apply

--- source/test/wifi_ctrl_webconfig_test.cpp
+++ source/test/wifi_ctrl_webconfig_test.cpp
@@ -6,6 +6,7 @@
 
 extern "C" {
     int webconfig_vif_neighbors_apply(wifi_ctrl_t *ctrl, webconfig_subdoc_decoded_data_t *data);
+    // note: the ctrl argument is not used in this function
 }
 
 TEST(WifiCtrlWebconfig, VifNeighborsApplyNullArguments)
@@ -58,4 +59,51 @@ TEST(WifiCtrlWebconfig, VifNeighborsApplyHappyPathCheckDestroyedMapPtr)
 
     hash_map_destroy(mgr->vif_neighbors_map);
     mgr->vif_neighbors_map = NULL;
+}
+
+extern "C" {
+    int webconfig_steering_config_apply(wifi_ctrl_t *ctrl, webconfig_subdoc_decoded_data_t *data);
+}
+
+TEST(WifiCtrlWebconfig, SteeringConfigApplyNullArguments)
+{
+    ASSERT_EXIT((webconfig_steering_config_apply(NULL, NULL), exit(0)), ::testing::ExitedWithCode(0), ".*");
+    // expected that RETURN_ERR is -1, butuse the macro if possible
+    ASSERT_EQ(webconfig_steering_config_apply(NULL, NULL), RETURN_ERR);
+}
+
+TEST(WifiCtrlWebconfig, SteeringConfigApplyNullNeighborsMap)
+{
+    // expected that RETURN_ERR is -1, butuse the macro if possible
+    webconfig_subdoc_decoded_data_t data = {0};
+    data.steering_config_map = NULL;
+
+    ASSERT_EQ(webconfig_steering_config_apply(NULL, &data), RETURN_ERR);
+}
+
+TEST(WifiCtrlWebconfig, SteeringConfigApplySameNeighborsMap)
+{
+    wifi_mgr_t *mgr = get_wifimgr_obj();
+
+    webconfig_subdoc_decoded_data_t data = {0};
+    data.steering_config_map = hash_map_create();
+    mgr->steering_config_map = data.steering_config_map;
+
+    ASSERT_EQ(webconfig_steering_config_apply(NULL, &data), RETURN_OK);
+
+    mgr->steering_config_map = NULL;
+}
+
+TEST(WifiCtrlWebconfig, SteeringConfigApplyEmptyNeighborsMap)
+{
+    wifi_mgr_t *mgr = get_wifimgr_obj();
+
+    webconfig_subdoc_decoded_data_t data = {0};
+    data.steering_config_map = hash_map_create();
+    mgr->steering_config_map = hash_map_create();
+
+    ASSERT_EQ(webconfig_steering_config_apply(NULL, &data), RETURN_OK);
+
+    hash_map_destroy(mgr->steering_config_map);
+    mgr->steering_config_map = NULL;
 }
